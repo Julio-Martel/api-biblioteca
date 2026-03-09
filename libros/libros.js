73e66cfa79f4c47148dd05f5098c2dc8f5967a61
Julio-Martel <julioramdom123@gmail.com>
2026-03-09 18:42:45 -0300

    se ha creado la base de datos de libros

---
+++ libros/libros.js
@@ -0,0 +1,24 @@
+const baseDeDatosLibros = [
+    {
+        codigo: 1,
+        titulo: "Attack on Titan",
+        autor: "Hajime Isayama",
+        disponible: true
+    },
+    
+    {
+        codigo: 2,
+        titulo: "La Elite de Hitler",
+        autor: "Chris McNab",
+        disponible: false            
+    },
+
+    {
+        codigo: 3,
+        titulo: "Bleach",
+        autor: "Tite Kubo",
+        disponible: true
+    }
+];
+
+module.exports = baseDeDatosLibros;
