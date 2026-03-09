5f04fc550edd2d08df5f6923ae2fcf6788414d1b
Julio-Martel <julioramdom123@gmail.com>
2026-03-09 18:57:14 -0300

    se ha aplicado el get para que me muestre todos los libros de la base de datos y tambien uno que filtra para buscar el libro mediante el codigo

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const express = require("express");
 const app = express();
 const baseDeDatosLibros = require("./libros/libros");
+
+app.use(express.json());
+
+app.get("/libros", (req,res) => {
+    res.json(baseDeDatosLibros);
+})
+
+app.get("/libros/:codigo",(req,res) => {
+    const codigo = parseInt(req.params.codigo);
+    const filtrarLibro = baseDeDatosLibros.findIndex(l => l.codigo === codigo);
+
+    if(filtrarLibro !== -1){
+        let libroEncontrado = baseDeDatosLibros[filtrarLibro];
+        res.status(200).json({
+            mensaje: "El libro filtrado es el siguiente",
+            libro: libroEncontrado
+        })
+    } else {
+        res.status(409).json(
+           { mensaje: "No existe libro con ese codigo"}
+        )
+    }
+})
+
+app.listen(3000,() => {
+    console.log("Servidor activo");
+})
